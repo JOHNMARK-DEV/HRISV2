4020233a78514aefd6c8704a6ac53b7fd4436a6e
unknown <[user>
2023-07-31 14:53:19 +0100

    pages

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import authRouter from './auth/authRouter'
 import userRouter from './system_config/user_management/userRouter' 
 import { supabase } from '../services/online/supabase'
-
-import EmployeeDashboard from '../views/Dashboard/EmployeeDashboard.vue'
-import Layout from '../layout/Layout.vue' 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
 
